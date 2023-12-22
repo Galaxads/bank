078a9b71aabfd563a28d9a62415cfab1fda66d74
Galaxads <114816670+Galaxads@users.noreply.github.com>
2023-12-22 12:33:14 +0300

    Add files via upload

---
+++ ConsoleApp5/Class1.cs
@@ -0,0 +1,111 @@
+﻿using System;   
+using System.Collections.Generic;  
+using System.Linq; 
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+class Chetvbanke
+{
+   public int nom;
+   private string fio = "";
+   public float sum;
+   private void otk(int nom,string fio, float sum) 
+   {
+        Console.WriteLine($"Введите номер счета (на данный момент номер счета {nom})"); 
+        nom=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Введите ФИО владельца (на данный момент ФИО {fio})");
+        fio = Console.ReadLine();
+        Console.WriteLine($"Введите сумму для пополнения(на данный момент сумма на счету {sum})");
+        sum = Convert.ToInt32(Console.ReadLine());
+        this.sum = this.sum + sum;
+        this.nom = nom;
+        this.fio = fio;
+   }
+   private void Out()  
+   { 
+    Console.WriteLine($"Ваш номер счета:{nom} \nФИО владельца счета:{fio} \nКоличество денег на счету:{sum}");
+   }
+   private void dob(float sum)
+   {
+     float iznachal = this.sum;//переменная для хранения изначального баланса
+     Console.WriteLine("На какую сумму пополнить счет?");
+     sum = Convert.ToInt32(Console.ReadLine()); 
+     sum = iznachal + sum;
+     Console.WriteLine($"Ваш обновленный баланс {sum}");
+     this.sum = sum;   
+   }                                                                                                  
+   private void umen(float sum)                                              
+   {                                        
+    float iznachal = this.sum;//переменная для хранения изначального баланса                                                                                
+        Console.WriteLine("На какую сумму снять со счета?");
+        sum = Convert.ToInt32(Console.ReadLine());
+        if (sum<=iznachal)
+        {
+          sum = iznachal - sum;
+          Console.WriteLine($"Ваш обновленный баланс: {sum}");
+          this.sum=sum; 
+        }
+    else
+    {
+     Console.WriteLine("Сумма для снятия превышает сумму баланса");
+    }
+   }                                                            
+   private void obnul()                           
+   {                                                        
+    sum= this.sum-this.sum;                                                   
+    Console.WriteLine($"Вы сняли все деньги с баланса.Ваш обновленный баланс {sum}");                               
+   }
+    public float perenos(int operachet,int schet2)
+    {
+        float balans1=this.sum;
+        
+        Console.WriteLine("Сколько денег перевести");
+        float perevod = Convert.ToInt32(Console.ReadLine());
+
+        if (perevod < balans1)
+        {
+            float itogshiclo = this.sum - perevod;
+            float balans2 = perevod;// деньги для нового счета
+            this.sum = itogshiclo;// остаток на счету 
+             
+            Console.WriteLine($"С счета {operachet} было переведено на счет {schet2}  {perevod}рублей.");
+            return (perevod);
+
+        }
+        else
+        {
+            Console.WriteLine("Сумма перевода больше чем денег на балансе");
+            return perevod = 0; ;
+        }
+    }
+    public void zapolneniecheta(int operachet)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            
+         
+            Out();
+            Console.WriteLine($"(нажмите 1)Вы хотите положить/(2)снять/(3)обнулить/(4)выполнить перевод или сменить счет/(5)Перезаполнить данные");
+            int otvet2 = Convert.ToInt32(Console.ReadLine());
+            int schet2; 
+
+            switch (otvet2)
+            {
+                case 1:
+                    dob(1); break;
+                case 2:
+                    umen(1); break;
+                case 3:
+                    obnul(); break;
+                case 4:
+                    return;
+                case 5:
+                    otk(this.nom, "1", this.sum);break;
+                
+            }
+        }
+    }
+}
+  
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int otritchet;//переменная для открытия счета
+                Console.WriteLine("Сколько открыть счетов?");  
+                otritchet = Convert.ToInt32(Console.ReadLine());
+                Chetvbanke[] bank = new Chetvbanke[otritchet+1];
+                for (int i = 1; i < bank.Length; i++)// цикл для создания n кол-во счетов
+                {
+                    bank[i] = new Chetvbanke();
+                    ((Chetvbanke)bank[i]).nom = i;// счет номера равен его номеру в массиве
+                }
+                for (int i = 0; bank.Length > 0; )
+                {
+                    Console.WriteLine($"На данный момент создано {otritchet} счетов .На какой счет перейти?");
+                    int operachet = Convert.ToInt32(Console.ReadLine());
+                     bank[operachet].zapolneniecheta(operachet);
+                Console.WriteLine("Если вы хотите сменить счет нажмите 1.Для перевода на другой счет нажмите 2");
+                  int otvet = Convert.ToInt32(Console.ReadLine());
+                 if (otvet == 1)
+                 {
+                    Console.WriteLine($"На данный момент создано {otritchet} счетов .На какой счет перейти?");
+                    operachet = Convert.ToInt32(Console.ReadLine());
+                    bank[operachet].zapolneniecheta(operachet);
+                 }
+                 else
+                 {
+                    Console.WriteLine($"На какой счет положить деньги?.");
+                    int schet2 = Convert.ToInt32(Console.ReadLine());
+                     ((Chetvbanke)bank[schet2]).sum = bank[operachet].perenos(operachet, schet2);  // метод возвращает новую сумму с последующим присваиванием
+                 }
+                }
+             
+            
+        }   
+    }      
+}
